1bb2e7c750248e4f8e0c499a23c8e00151c4fdac
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2021-06-25 23:51:20 +0100

    modified js

--- keyboard.js
+++ keyboard.js
@@ -15,9 +15,117 @@ const keyboard = {
     capslock: false,
   },
 
-  init() {},
+  init() {
+    // Create main element
+    this.elements.main = document.createElement("div");
+    this.elements.keysContainer = document.createElement("div");
 
-  _createKeys() {},
+    // Setup main elements
+    this.elements.main.classList.add("keyboard", "1keyboard--hidden");
+    this.elements.keysContainer.classList.add("keyboard_keys");
+
+    // Add to DOM
+    this.elements.main.appendChild(this.elements.keysContainer);
+    document.body.appendChild(this.elements.main);
+  },
+
+  _createKeys() {
+    const fragment = document.createDocumentFragment();
+    const keyLayout = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "0",
+      "backspace",
+      "q",
+      "w",
+      "e",
+      "r",
+      "t",
+      "y",
+      "u",
+      "i",
+      "o",
+      "p",
+      "caps",
+      "a",
+      "s",
+      "d",
+      "f",
+      "g",
+      "h",
+      "j",
+      "k",
+      "l",
+      "enter",
+      "done",
+      "z",
+      "x",
+      "c",
+      "v",
+      "b",
+      "n",
+      "m",
+      ",",
+      ".",
+      "?",
+      "space",
+    ];
+
+    // Creates HTML for an icon
+    const createIconHTML = (icon_name) => {
+      return `<i class="material-icons">${icon_name}</i>`;
+    };
+
+    keyLayout.forEach((key) => {
+      const keyElement = document.createElement("button");
+      const insertLineBreak =
+        ["backspace", "p", "enter", "?"].indexOf(key) !== -1;
+
+      // Add attributes/classes
+      keyElement.setAttribute("type", "button");
+      keyElement.classList.add("keyboard_key");
+
+      switch (key) {
+        case "backspace":
+          keyElement.classList.add("keyboard_key-wide");
+          keyElement.innerHTML = createIconHTML("backspace");
+
+          keyElement.addEventListener("click", () => {
+            this.properties.value = this.properties.value.substring(
+              0,
+              this.properties.value.length - 1
+            );
+            this._triggerEvent("oninput");
+          });
+
+          break;
+
+        case "caps":
+          keyElement.classList.add(
+            "keyboard_key-wide",
+            "keyboard_key-activatable"
+          );
+          keyElement.innerHTML = createIconHTML("keyboard_capslock");
+
+          keyElement.addEventListener("click", () => {
+            this._toggleCapslock();
+            keyElement.classList.toggle(
+              "keyboard_key-active",
+              this.properties.capsLock
+            );
+          });
+
+          break;
+      }
+    });
+  },
 
   _triggerEvent(handlerName) {
     console.log("Event triggered! Event Name: " + handlerName);
@@ -27,7 +135,11 @@ const keyboard = {
     console.log("Caps Lock Toggled!");
   },
 
-  open() {},
+  open(initialValue, oninput, onclose) {},
 
   close() {},
 };
+
+window.addEventListener("DOMContentLoaded", function () {
+  keyboard.init();
+});
